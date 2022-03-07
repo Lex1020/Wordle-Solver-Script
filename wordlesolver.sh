0eaaa49dd5311b444a9789fe6ddb48fedfc2ee56
Daniel Teel <daniel92374@gmail.com>
2022-03-07 13:42:49 -0500

    Wordle Solver Update with while loop

--- wordlesolver.sh
+++ wordlesolver.sh
@@ -10,12 +10,12 @@ if [ -f /home/$user/wordlewords ]; then
 		echo "Downloading file."
 		wget -q --show-progress https://raw.githubusercontent.com/tabatkins/wordle-list/main/words
 		mv words /home/$user/wordlewords
+		sort wordlewords
 		echo "Dictonary downloaded. Proceefing with program."
 		echo "=============================================="
 fi
 echo "Please enter the first range of letters. (Note: enter them as a-z, or if you are eliminating letters then enter them as a range like a-df-z)"
 read letter1
-#echo "Your first quess is $letter1"
 echo "Please enter the range of your second letter."
 read letter2
 echo "Please enter the range of your third letter." 
@@ -36,64 +36,30 @@ if [ $answer1 == Y ]; then
 	tr -d '\n' < /tmp/letters.txt | tee /tmp/letters.txt
 	EL=$(cat /tmp/letters.txt)
 	grep -Evi "([$EL])" /tmp/wordleguesses.txt > /tmp/wordleguesses2.txt
+	mv /tmp/wordleguesses2.txt /tmp/wordleguesses.txt
 fi
 echo \n
 echo "Are there any letters you know exist, but not the place? (Y/N)"
 read answer
 if [ $answer == Y ]; then
+	cp /tmp/wordleguesses.txt /tmp/words
 	echo "How many letters? (Enter 1-5)"
-	read number_letters
-	if [ $number_letters = 5 ]; then
-		echo "Letter One"
-		read letterA
-		echo "Letter two"
-		read letterB
-		echo "Letter three"
-		read letterC
-		echo "Letter Four" 
-		read letterD
-		echo "Letter Five"
-		read letterE
-		grep -Ei "([$letterA])" /tmp/wordleguesses2.txt | grep -Ei "([$letterB])" | grep -Ei "([$letterC])" | grep -Ei "([$letterD])" | grep -Ei "([$letterE])" > /tmp/wordleguesses.txt
-		elif [ $number_letters = 4 ]; then
-			echo "Letter One"
-			read letterA
-			echo "Letter Two"
-			read letterB
-			echo "Letter Three"
-			read letterC
-			echo "Letter Four"
-			read letterD
-			grep -Ei "([$letterA])" /tmp/wordleguesses2.txt | grep -Ei "([$letterB])" | grep -Ei "([$letterC])" | grep -Ei "([$letterD])" > /tmp/wordleguesses.txt
-			elif [ $number_letters = 3 ]; then 
-				echo "Letter One" 
-				read letterA
-				echo "Letter Two"
-				read letterB
-				echo "Letter Three"
-				read letterC
-				grep -Ei "([$letterA])" /tmp/wordleguesses2.txt | grep -Ei "([$letterB])" | grep -Ei "([$letterC])" > /tmp/wordleguesses.txt
-				elif [ $number_letters = 2 ]; then 
-					echo "Letter One"
-					read letterA
-					echo "Letter Two" 
-					read letterB
-					grep -Ei "([$letterA])" /tmp/wordleguesses2.txt | grep -Ei "([$letterB])" > /tmp/wordleguesses.txt
-					elif [ $number_letters = 1 ]; then
-						echo "Letter One"
-						read letterA
-						grep -Ei "([$letterA])" /tmp/wordleguesses2.txt > /tmp/wordleguesses.txt
-
-	fi
-	elif [ $answer == N ]; then
-	cp /tmp/wordleguesses2.txt /tmp/wordleguesses.txt  
-	#fi
+	read i
+	while [ $i -gt 0 ];
+		do 
+			echo "What is letter $i"
+			read letter
+			grep -Ei "([$letter])" /tmp/words >> /tmp/wordleguesses2.txt
+			mv /tmp/wordleguesses2.txt /tmp/words
+			cp /tmp/words /tmp/wordleguesses.txt
+			((i--))
+		done
 fi
 echo "Your Wordle suggestions are:"
 echo "============================================================="
 cat /tmp/wordleguesses.txt
 echo "============================================================="
-rm /tmp/wordleguesses*.txt 2>&1 1> /dev/null
+rm /tmp/wordleguesses*.txt /tmp/words 2>&1 1> /dev/null
 echo "Did you solve the word? (Y/N)"
 read solve
 if [ $solve == Y ]; then 
